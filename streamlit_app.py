787f62dde94386ad27e888bc1c66dc494b6f1211
belvaaqilah <khalissanadya85@gmail.com>
2025-05-22 15:01:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,11 @@ st.title("🎈 khalissanadyamukti28maps")
 st.write(
     "khalissanadyamukti28maps."
 )
+st.image("655793c8-2ea1-4d35-ab29-24066561fd3a.jpeg")
+st.harder("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
+
+if (angka % 2) == 0;
+ st.write(f"{angka} adalah bilangan genap")
+else:
+ st.write(f"{angka} adalah bilangan ganjil")
