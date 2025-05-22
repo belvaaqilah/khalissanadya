cdeea76301a6abb9c296a045e6a113e21145bc3e
belvaaqilah <khalissanadya85@gmail.com>
2025-05-22 14:18:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 belvaaqilah")
-st.write(belvaaailah)
-    "belvaaqilah."
+st.title("🎈 khalissanadyamukti28maps")
+st.write(khalissanadyamukti28maps)
+    "khalissanadyamukti28maps."
 )
