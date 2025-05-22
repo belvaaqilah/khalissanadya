1f7de5033c6e30632932899b1a770a5a423bcdc8
belvaaqilah <khalissanadya85@gmail.com>
2025-05-22 15:03:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.write(
     "khalissanadyamukti28maps."
 )
 st.image("655793c8-2ea1-4d35-ab29-24066561fd3a.jpeg")
-st.harder("Aplikasi Mengecek Nilai Genap/Ganjil")
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
 angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
 
 if (angka % 2) == 0:
